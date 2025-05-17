e32159f78043c0054fa7ba13836f72cd4769a015
mindahun21 <mndahun0@gmail.com>
2025-05-17 21:48:16 +0300

    performance2

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -16,7 +16,9 @@ SELECT
 FROM Booking b
 JOIN "User" u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' AND b.start_date >= CURRENT_DATE;
+
 
 
 
