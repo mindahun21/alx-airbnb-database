64a4be07a981ee3c294f4fcc0a3cc7cb5b0706ac
mindahun21 <mndahun0@gmail.com>
2025-05-17 21:35:57 +0300

    subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,20 @@
+
+-- a query to find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT *
+FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+-- a correlated subquery to find users who have made more than 3 bookings.
+
+SELECT u.*
+FROM "User" u
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
